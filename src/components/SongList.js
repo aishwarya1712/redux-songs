46af066024a9d0cef46b20597c99ab9e6c19574f
Aishwarya Sriram <98.aishu@gmail.com>
2022-03-03 16:42:20 +0530

    Called action creator from react component

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -1,13 +1,16 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {selectSong} from "../actions"
 
 class SongList extends Component{
+    
     renderedList() {
+        console.log(this.props)
         return this.props.songs.map((song, key) => {
             return (
                 <div className="item" key={key}>
                     <div className="right floated content">
-                        <button className="ui button primary">Select</button>
+                        <button className="ui button primary" onClick={() => this.props.selectSong(song)}>Select</button>
                     </div>
                     <div className="content">
                         {song.title}
@@ -24,14 +27,16 @@ class SongList extends Component{
 }
 
 
+// mapStateToProps is called with new state whenever state changes due to the reducers being called.
 // by convention we call this function mapStateToProps, but it can be called anything
 // called with all of the state in redux store i.e list of all songs and currently selected song
 const mapStateToProps = (state) => {
 
-    // console.log(state);
+    console.log(state);
     // return state; // --------> will return both list of all songs and selected song
 
     return {songs: state.songs} // since in SongList component we only need list of all songs, we don't need the selectedSong
 }
 
-export default connect(mapStateToProps)(SongList);
+// passing selectSong action creator also to connect
+export default connect(mapStateToProps, {selectSong: selectSong})(SongList);
