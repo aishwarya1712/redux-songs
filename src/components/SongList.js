1729c58600603c7cbf3bf5f15fcbe48ab80bc572
Aishwarya Sriram <98.aishu@gmail.com>
2022-03-03 11:09:27 +0530

    Used redux state in react component, set up semantic ui classnames

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -2,11 +2,36 @@ import React, {Component} from "react";
 import {connect} from "react-redux";
 
 class SongList extends Component{
+    renderedList() {
+        return this.props.songs.map((song, key) => {
+            return (
+                <div className="item" key={key}>
+                    <div className="right floated content">
+                        <button className="ui button primary">Select</button>
+                    </div>
+                    <div className="content">
+                        {song.title}
+                    </div>
+                </div>
+            )
+        })
+    }
     render(){
-        return <div>
-            Song List
+        return <div className="ui divided list">
+            {this.renderedList()}
         </div>
     }
 }
 
-export default connect()(SongList);
+
+// by convention we call this function mapStateToProps, but it can be called anything
+// called with all of the state in redux store i.e list of all songs and currently selected song
+const mapStateToProps = (state) => {
+
+    // console.log(state);
+    // return state; // --------> will return both list of all songs and selected song
+
+    return {songs: state.songs} // since in SongList component we only need list of all songs, we don't need the selectedSong
+}
+
+export default connect(mapStateToProps)(SongList);
